a61e520cf9b5ad82cd73026e8eb3b0a4f9dc2bdd
RootsElectronics <38021901+RootsElectronics@users.noreply.github.com>
2022-12-10 02:36:16 +0000

    Added kinematics C script and some other stuff
    Kinematics script, written for now as a C program that outputs the desired codes for the SSC-32 based on X,Y,Z positions.

--- Kinematics/Kinematics_Script_Finished.m
+++ Kinematics/Kinematics_Script_Finished.m
@@ -8,18 +8,19 @@
 %
 % a and b needs to be under 165 to stay within working envelope
 %% Assiging Variables
-a =100 ;
-b = 50;
-alpha = a+5;
-beta = b + 176;
+% a =100 ;
+% b = 50;
+% alpha = a+5;
+% beta = b + 176;
+alpha = -40
+beta = 477.5
+%% Generating coordinates for box/grid
+% Box_BLHS = [a , b] % box bottom left hand side
+% Box_BRHS = [a+240, b]
+% Box_TLHS = [a , b+340]
+% Box_TRHS = [a+240, b+340]
 
-%% NOT NEEDED Generating coordinates for box/grid
-Box_BLHS = [a , b] % box bottom left hand side
-Box_BRHS = [a+240, b]
-Box_TLHS = [a , b+340]
-Box_TRHS = [a+240, b+340]
-
-%% Not needed in code Generating coordinates for individual boxes/trays
+%% Generating coordinates for individual boxes/trays
 %taking alpha and beta as the reference point
 % itl = individual tray length
 % itw = individual tray width
@@ -35,24 +36,24 @@ c = 5;
 % using the Top LHS for reference point (alpha,beta)
 ref = [alpha,beta]
 
-T1 = [ref,ref+[itl,0] ; ref + [0,-itw] , ref+[itl,-itw]]
-T2 = [ref+[itl+c,0], ref + [itl+itl+c,0] ; ref+[itl+c,-itw] , ref+[itl+itl+c,-itw]]
-T3 = [ref+[itl+itl+c+c,0], ref+[itl+itl+itl+c+c,0]; ref+[itl+itl+c+c,-itw], ref+[itl+itl+itl+c+c,-itw]]
+T1 = [ref,ref+[0,itl] ; ref + [itw,0] , ref+[itw,itl]]
+T2 = [ref+[0,itl+c], ref + [0,itl+itl+c] ; ref+[itw,itl+c] , ref+[itw,itl+itl+c]]
+T3 = [ref+[0,itl+itl+c+c], ref+[0,itl+itl+itl+c+c]; ref+[itw,itl+itl+c+c], ref+[itw,itl+itl+itl+c+c]]
 % T4 = [ref+[itl+c+itl+c+itl+c,0],ref+[itl+c+itl+c+itl+c+itl,0] ; ref+[itl+c+itl+c+itl+c,-itw], ref+[itl+c+itl+c+itl+c+itl,itw]]
-T4 = [ref+[0,-itw-c], ref+[itl,-itw-c]; ref+[0,-itw-c-itw] , ref+[itl,-itw-c-itw]]
-T5 = [ref+[itl+c,-itw-c], ref+[itl+itl+c,-itw-c] ; ref+[itl+c,-itw-c-itw], ref+[itl+itl+c,-itw-c-itw]]
-T6 = [ref+[itl+itl+c+c,-itw-c],ref+[itl+itl+itl+c+c,-itw-c] ; ref+[itl+itl+c+c,-itw-c-itw] , ref+[itl+itl+itl+c+c,-itw-c-itw]]
+T4 = [ref+[itw+c,0], ref+[itw+c,itl]; ref+[itw+c+itw,0] , ref+[itw+c+itw,itl]]
+T5 = [ref+[itw+c,itl+c], ref+[itw+c,itl+itl+c] ; ref+[itw+c+itw,itl+c], ref+[itw+c+itw,itl+itl+c]]
+T6 = [ref+[itw+c,itl+itl+c+c],ref+[itw+c,itl+itl+itl+c+c] ; ref+[itw+c+itw,itl+itl+c+c] , ref+[itw+c+itw,itl+itl+itl+c+c]]
 % T8 = [ref+[itl+c+itl+c+itl+c,-itw-c], ref+[itl+c+itl+c+itl+c+itl,-itw-c]; ref+[itl+c+itl+c+itl+c,-itw-c-itw],ref+[itl+c+itl+c+itl+c+itl,-itw-c-itw]]
-T7 = [ref+[0,-itw-c-itw-c],ref+[itl,-itw-c-itw-c]; ref+[0,-itw-c-itw-c-itw], ref+[itl,-itw-c-itw-c-itw]]
-T8 = [ref+[itl+c,-itw-c-itw-c], ref+[itl+itl+c,-itw-c-itw-c]; ref+[itl+c,-itw-c-itw-c-itw], ref+[itl+itl+c,-itw-c-itw-c-itw]]
-T9 = [ref+[itl+itl+c+c,-itw-c-itw-c], ref+[itl+itl+itl+c+c,-itw-c-itw-c]; ref+[itl+itl+c+c,-itw-c-itw-c-itw], ref+[itl+itl+itl+c+c,-itw-c-itw-c-itw]]
-T_EX_1 = [ref+[itl+c+itl+c+itl+c,-itw-c-itw-c], ref+[itl+c+itl+c+itl+c+itl,-itw-c-itw-c]; ref+[itl+c+itl+c+itl+c,-itw-c-itw-c-itw] , ref+[itl+c+itl+c+itl+c+itl,-itw-c-itw-c-itw]]
+T7 = [ref+[itw+c+itw+c,0],ref+[itw+c+itw+c,itl]; ref+[itw+c+itw+c+itw,0], ref+[itw+c+itw+c+itw,itl]]
+T8 = [ref+[itw+c+itw+c,itl+c], ref+[itw+c+itw+c,itl+itl+c]; ref+[itw+c+itw+c+itw,itl+c], ref+[itw+c+itw+c+itw,itl+itl+c]]
+T9 = [ref+[itw+c+itw+c,itl+itl+c+c,], ref+[itw+c+itw+c,itl+itl+itl+c+c]; ref+[itw+c+itw+c+itw,itl+itl+c+c,], ref+[itw+c+itw+c+itw,itl+itl+itl+c+c]]
+T_EX_1 = [ref+[itw+c+itw+c,itl+c+itl+c+itl+c], ref+[itw+c+itw+c,itl+c+itl+c+itl+c+itl]; ref+[itw+c+itw+c+itw,itl+c+itl+c+itl+c] , ref+[itw+c+itw+c+itw,itl+c+itl+c+itl+c+itl]]
 %one extra tray needed for misc resistors,will be named T_EX
 % change of design means this is now redundant :T_EX = [ref+[itl+c+(itl/2),c+itw], ref+[itl+c+(itl/2)+itl,c+itw] ; ref+[itl+c+(itl/2),c] , ref+[itl+c+(itl/2)+itl,c]]
-T10 = [ref+[0,-itw-c-itw-c-itw-c],ref+[itl,-itw-c-itw-c-itw-c]; ref+ [0,-itw-c-itw-c-itw-c-itw], ref+[itl,-itw-c-itw-c-itw-c-itw]]
-T11 = [ref+[itl+c,-itw-c-itw-c-itw-c], ref+[itl+c+itl,-itw-c-itw-c-itw-c]; ref+[itl+c,-itw-c-itw-c-itw-c-itw], ref+[itl+c+itl,-itw-c-itw-c-itw-c-itw]]
-T12 = [ref+[itl+itl+c+c,-itw-c-itw-c-itw-c], ref+[itl+itl+itl+c+c,-itw-c-itw-c-itw-c]; ref+[itl+itl+c+c,-itw-c-itw-c-itw-c-itw], ref+[itl+itl+itl+c+c,-itw-c-itw-c-itw-c-itw]]
-T_EX_2 = [ref+[itl+c+itl+c+itl+c,-itw-c-itw-c-itw-c], ref+[itl+c+itl+c+itl+c+itl,-itw-c-itw-c-itw-c]; ref+[itl+c+itl+c+itl+c,-itw-c-itw-c-itw-c-itw], ref+[itl+c+itl+c+itl+c+itl,-itw-c-itw-c-itw-c-itw]]
+T10 = [ref+[itw+c+itw+c+itw+c,0],ref+[itw+c+itw+c+itw+c,itl]; ref+[itw+c+itw+c+itw+c+itw,0], ref+[itw+c+itw+c+itw+c+itw,itl]]
+T11 = [ref+[itw+c+itw+c+itw+c,itl+c], ref+[itw+c+itw+c+itw+c,itl+c+itl]; ref+[itw+c+itw+c+itw+c+itw,itl+c], ref+[itw+c+itw+c+itw+c+itw,itl+c+itl]]
+T12 = [ref+[itw+c+itw+c+itw+c,itl+c+itl+c], ref+[itw+c+itw+c+itw+c,itl+itl+itl+c+c]; ref+[itw+c+itw+c+itw+c+itw,itl+itl+c+c,], ref+[itw+c+itw+c+itw+c+itw,itl+itl+itl+c+c]]
+T_EX_2 = [ref+[itw+c+itw+c+itw+c,itl+c+itl+c+itl+c], ref+[itw+c+itw+c+itw+c,itl+c+itl+c+itl+c+itl]; ref+[itw+c+itw+c+itw+c+itw,itl+c+itl+c+itl+c], ref+[itw+c+itw+c+itw+c+itw,itl+c+itl+c+itl+c+itl]]
 %% finding the midpoint of each tray for forward kinematics
 %for i<13;
  %   i = 1
@@ -61,22 +62,21 @@ T_EX_2 = [ref+[itl+c+itl+c+itl+c,-itw-c-itw-c-itw-c], ref+[itl+c+itl+c+itl+c+itl
 %tried using for loops but couldn't get it to work
 
 % M_T = midpoint of T()
-M_T1 = [(T1(1)+T1(5))/2,(T1(3)+T1(7))/2]
-M_T2 = [(T2(1)+T2(5))/2,(T2(3)+T2(7))/2]
-M_T3 = [(T3(1)+T3(5))/2,(T3(3)+T3(7))/2]
-M_T4 = [(T4(1)+T4(5))/2,(T4(3)+T4(7))/2]
-M_T5 = [(T5(1)+T5(5))/2,(T5(3)+T5(7))/2]
-M_T6 = [(T6(1)+T6(5))/2,(T6(3)+T6(7))/2]
-M_T7 = [(T7(1)+T7(5))/2,(T7(3)+T7(7))/2]
-M_T8 = [(T8(1)+T8(5))/2,(T8(3)+T8(7))/2]
-M_T9 = [(T9(1)+T9(5))/2,(T9(3)+T9(7))/2]
-M_T10 = [(T10(1)+T10(5))/2,(T10(3)+T10(7))/2]
-M_T11 = [(T11(1)+T11(5))/2,(T11(3)+T11(7))/2]
-M_T12 = [(T12(1)+T12(5))/2,(T12(3)+T12(7))/2]
-M_T_EX_1 = [(T_EX_1(1)+T_EX_1(5))/2,(T_EX_1(3)+T_EX_1(7))/2]
-M_T_EX_2 = [(T_EX_2(1)+T_EX_2(5))/2,(T_EX_2(3)+T_EX_2(7))/2]
-
-%% NOT NEEDED IN CODE finding the length between base and midpoint of the tray
+M_T1 = [(T1(1)+T1(2))/2,(T1(3)+T1(7))/2]
+M_T2 = [(T2(1)+T2(2))/2,(T2(3)+T2(7))/2]
+M_T3 = [(T3(1)+T3(2))/2,(T3(3)+T3(7))/2]
+M_T4 = [(T4(1)+T4(6))/2,(T4(3)+T4(7))/2]
+M_T5 = [(T5(1)+T5(2))/2,(T5(3)+T5(7))/2]
+M_T6 = [(T6(1)+T6(2))/2,(T6(3)+T6(7))/2]
+M_T7 = [(T7(1)+T7(2))/2,(T7(3)+T7(7))/2]
+M_T8 = [(T8(1)+T8(2))/2,(T8(3)+T8(7))/2]
+M_T9 = [(T9(1)+T9(2))/2,(T9(3)+T9(7))/2]
+M_T10 = [(T10(1)+T10(2))/2,(T10(3)+T10(7))/2]
+M_T11 = [(T11(1)+T11(2))/2,(T11(3)+T11(7))/2]
+M_T12 = [(T12(1)+T12(2))/2,(T12(3)+T12(7))/2]
+M_T_EX_1 = [(T_EX_1(1)+T_EX_1(2))/2,(T_EX_1(3)+T_EX_1(7))/2]
+M_T_EX_2 = [(T_EX_2(1)+T_EX_2(2))/2,(T_EX_2(3)+T_EX_2(7))/2]
+%% finding the length between base and midpoint of the tray
 
 L1 = sqrt(M_T1(1)^2 + M_T1(2)^2)
 L2 = sqrt(M_T2(1)^2 + M_T2(2)^2)
@@ -95,11 +95,12 @@ L_EX_2 = sqrt(M_T_EX_2(1)^2 + M_T_EX_2(2)^2)
 T_L = [L1,L2,L3,L4;L5,L6,L7,L8;L9,L10,L11,L12] % putting all of them into a table
 
 
-%% WE NEED THIS Finding the angle between base and midpoint of tray
-
-theta_1_1 = atand(M_T1(2)/M_T1(1)) % in degrees
-theta_1_2 = atand(M_T2(2)/M_T2(1))
-theta_1_3 = atand(M_T3(2)/M_T3(1))
+%% Finding the angle between base and midpoint of tray
+% for angle calculations, best to have the modulus of coordinates for
+% T1,2,3
+theta_1_1 = atand(M_T1(2)/abs(M_T1(1))) % in degrees
+theta_1_2 = atand(M_T2(2)/abs(M_T2(1)))
+theta_1_3 = atand(M_T3(2)/abs(M_T3(1)))
 theta_1_4 = atand(M_T4(2)/M_T4(1))
 theta_1_5 = atand(M_T5(2)/M_T5(1))
 theta_1_6 = atand(M_T6(2)/M_T6(1))
@@ -129,7 +130,6 @@ d3 = 85.725; % wrist to gripper length
 
 h = sqrt(d2^2-(d1-d3)^2)
 theta_3 = asind(h/d2) % if the distance to reach is greater than h, theta 2 will be set to this value
-
 %% theta 2 calcs
 % if L is less than h, theta 2 will be 0, otherwise theta 3 will be
 % constant and theta 2 will be determined
@@ -155,10 +155,10 @@ theta_2_3 = atand(x_3/d1)
 theta_2_4 = atand(x_4/d1)
 theta_2_5 = atand(x_5/d1)
 theta_2_6 = atand(x_6/d1)
-theta_2_7 = 0
+theta_2_7 = atand(x_7/d1)
 theta_2_8 = atand(x_8/d1)
 theta_2_9 = atand(x_9/d1) 
-theta_2_10 = 0
+theta_2_10 = atand(x_10/d1)
 theta_2_11= atand(x_11/d1)
 theta_2_12 = atand(x_12/d1)
 theta_2_EX_1 = atand(x_EX_1/d1)
@@ -171,10 +171,10 @@ theta_3_3 = theta_3
 theta_3_4 = theta_3
 theta_3_5 = theta_3
 theta_3_6 = theta_3
-theta_3_7 = atand(L9/d1)
+theta_3_7 = theta_3
 theta_3_8 = theta_3
 theta_3_9 = theta_3
-theta_3_10 = atand(L9/d1)
+theta_3_10 = theta_3
 theta_3_11= theta_3
 theta_3_12 = theta_3
 theta_3_EX_1 = theta_3
---
+++ Kinematics/kinematics.c
@@ -0,0 +1,103 @@
+// C program for testing kinematics for AL5D robot arm
+// This program is for testing purposes only, it is not intended to be used in a production environment
+// This code is part of the GDIP AL5D robot arm project
+// 
+// Author: J. O'Hare
+// Student ID: 20023827
+// GDIP GROUP: 7D
+// Version 1.0;
+// Date 10/12/2022
+//
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#define _USE_MATH_DEFINES
+#define BASE_HGT 67.31      //base hight 2.65"
+#define SHOULDER 146.05       //shoulder-to-elbow "bone" 5.75"
+#define ELBOW 276.225        //elbow-to-wrist "bone" 7.375"
+#define GRIPPER 85.72      //gripper (wrist rotate mechanism) length 3.94"
+//float to long
+#define ftl(x) ((x)>=0?(long)((x)+0.5):(long)((x)-0.5))  
+// Define constants
+float sh_sq = SHOULDER*SHOULDER;
+float el_sq = ELBOW*ELBOW;
+float robotX = 0, robotY = 0, robotZ = 0;
+//function prototypes
+float degrees(float rad);
+float radians(float deg);
+char* getArmCode( float x, float y, float z, float grip_angle_d);
+int main()
+{
+    float x, y, z;
+    printf("\n\n");
+    printf("Enter x");
+    scanf("%f", &x);
+    printf("Enter y");
+    scanf("%f", &y);
+    printf("Enter z");
+    scanf("%f", &z);
+    //gripper position set to 45 degrees, optimum for measuring when z is 0
+    printf("%s", getArmCode(x, y, z, 45));
+    return 0;
+}
+
+// function to convert radians to degrees
+float degrees(float rad)
+{
+    return (rad * 180.0 / M_PI);
+}
+
+float radians(float deg)
+{
+    return (deg * M_PI / 180.0);
+}
+
+char* getArmCode( float x, float y, float z, float grip_angle_d )
+{   
+    //grip angle in radians for use in calculations
+    float grip_angle_r = radians( grip_angle_d );    
+    //Adjustment so that the gripper hits the floor at z=0;
+    z += 170;
+    //Base angle and radial distance from x,y coordinates
+    float d = sqrt( x*x + y*y );    //distance from base center to x,y coordinates
+    float phi=90.00;
+    float bas_angle_r = atan2( x, y );
+    float L = sqrt(( x * x ) + ( y * y ));
+    //L is y coordinate for the arm
+    L -= cos(phi)*GRIPPER;
+    y = L;
+    //Grip offsets calculated based on grip angle
+    float grip_off_z = ( sin( grip_angle_r )) * GRIPPER;
+    float grip_off_y = ( cos( grip_angle_r )) * GRIPPER;
+    //Wrist position
+    float wrist_z = ( z - grip_off_z ) - BASE_HGT;
+    wrist_z -= sin(phi)*GRIPPER;
+    float wrist_y = y - grip_off_y;
+    //Shoulder to wrist distance ( AKA sw )
+    float s_w = ( wrist_z * wrist_z ) + ( wrist_y * wrist_y );
+    float s_w_sqrt = sqrt( s_w );
+    //s_w angle to ground
+    //float a1 = atan2( wrist_y, wrist_z );
+    float a1 = atan2( wrist_z, wrist_y );
+    //s_w angle to SHOULDER
+    float a2 = acos((( sh_sq - el_sq ) + s_w ) / ( 2 * SHOULDER * s_w_sqrt ));
+    //shoulder angle
+    float shl_angle_r = a1 + a2;
+    float shl_angle_d = degrees( shl_angle_r );
+    //elbow angle
+    float elb_angle_r = acos(( sh_sq + el_sq - s_w ) / ( 2 * SHOULDER * ELBOW ));
+    float elb_angle_d = degrees( elb_angle_r );
+    float elb_angle_dn = -( 180.0 - elb_angle_d );
+    //wrist angle
+    float wri_angle_d = ( grip_angle_d - elb_angle_dn ) - shl_angle_d;
+    //Servo pulses
+    float bas_servopulse = 1500.0 - (( degrees( bas_angle_r )) * 11.11 );
+    float shl_servopulse = 1500.0 + (( shl_angle_d - 90.0 ) * 6.6 );
+    float elb_servopulse = 1500.0 -  (( elb_angle_d - 90.0 ) * 6.6 );
+    float wri_servopulse = 1500 - ( wri_angle_d  * 11.1 );
+    //Set servo pulses
+    char *output;
+    //Added some offsets here, not sure if they are correct, they seem to work though
+    sprintf(output, "#0P%u#1P%u#2P%u#3P%uT100\r", ftl(1500-bas_servopulse+500), ftl(shl_servopulse), ftl(elb_servopulse), ftl(wri_servopulse+400));
+    return output;
+}
